e0e6b39db9ee4ac9861fbcdbbc0886c287b753d6
gary <gary.white@lodgit.com>
2021-11-04 16:49:30 +0100

    not finished

--- kataNonCon.js
+++ kataNonCon.js
@@ -1,5 +1,13 @@
 function firstNonConsecutive (arr) {
-    return arr.length;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[0] == arr[1] - 1) {
+            arr[0] = arr[0] + arr[1]
+        } else {
+            return null;
+        }
+
+    }
+    // return arr.length;
 }
 
 console.log(firstNonConsecutive([1,2,3,4,6,7,8]));
